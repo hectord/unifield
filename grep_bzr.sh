05fc107806b6aedc22a98554e79a854672f2e1a7
Hector Davensburg <hector.davensburg@gmail.com>
2016-08-02 12:28:31 +0200

    New tool to merge the trunk with server AND web branches

--- grep_bzr.sh
+++ grep_bzr.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-if [[ $# != 1 && $# != 2 ]]
+if [[ $# != 2 && $# != 3 ]]
 then
-    echo "You should set the branch / revision!"
+    echo "You should set the name (web or server), the branch and (optionally), the revision!"
     exit 1
 fi
 
 TMPDIR_BRANCH=/tmp/tmp_branch
 
-rm -rf server/*
+rm -rf $1/*
 rm -rf $TMPDIR_BRANCH || true
 
 if [[ $# == 1 ]]
 then
-    bzr checkout --lightweight $1 $TMPDIR_BRANCH
+    bzr checkout --lightweight $2 $TMPDIR_BRANCH
 else
-    bzr checkout --lightweight -r $2 $1 $TMPDIR_BRANCH
+    bzr checkout --lightweight -r $3 $2 $TMPDIR_BRANCH
 fi
 
 
-cp -R $TMPDIR_BRANCH/* server/
+cp -R $TMPDIR_BRANCH/* $1/
 
