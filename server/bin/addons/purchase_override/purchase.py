3f94aea726467ab04df464f6c61551aedac68ef9
Hector Davensburg <hector.davensburg@gmail.com>
2016-08-02 18:01:43 +0200

    WiP purchase order

--- server/bin/addons/purchase_override/purchase.py
+++ server/bin/addons/purchase_override/purchase.py
@@ -1092,6 +1092,8 @@ stock moves which are already processed : '''
         todo = []
         reset_soq = []
 
+        print "BEGINNING"
+
         for po in self.browse(cr, uid, ids, context=context):
             line_error = []
             if po.order_type == 'regular':
@@ -1104,6 +1106,8 @@ stock moves which are already processed : '''
             if len(line_error) > 0:
                 errors = ' / '.join(str(x) for x in line_error)
                 raise osv.except_osv(_('Error !'), _('You cannot have a purchase order line with a 0.00 Unit Price or 0.00 Subtotal. Lines in exception : %s') % errors)
+
+            print "CHECK PRICELIST"
 
             # Check if the pricelist of the order is good according to currency of the partner
             pricelist_ids = self.pool.get('product.pricelist').search(cr, uid,
@@ -1118,11 +1122,15 @@ stock moves which are already processed : '''
             if po.order_type == 'purchase_list' and po.amount_total == 0:  # UFTP-69
                 raise osv.except_osv(_('Error'), _('You can not validate a purchase list with a total amount of 0.'))
 
+            print "ITERATE OVER LINES"
+
             for line in po.order_line:
                 if line.state=='draft':
                     todo.append(line.id)
                 if line.soq_updated:
                     reset_soq.append(line.id)
+
+            print "END ITERATION"
 
             message = _("Purchase order '%s' is validated.") % (po.name,)
             self.log(cr, uid, po.id, message)
@@ -1132,14 +1140,21 @@ stock moves which are already processed : '''
             # hook for corresponding Fo update
             self._hook_confirm_order_update_corresponding_so(cr, uid, ids, context=context, po=po)
 
+        print "ACTION CONFIRM"
+
         po_line_obj.action_confirm(cr, uid, todo, context)
+        print "PO LINE WRITE"
         po_line_obj.write(cr, uid, reset_soq, {'soq_updated': False,}, context=context)
 
+        print "PO CONFIRM"
         self.write(cr, uid, ids, {'state' : 'confirmed',
                                   'validator' : uid,
                                   'date_confirm': strftime('%Y-%m-%d')}, context=context)
 
+        print "CHECK DISTRIB"
         self.check_analytic_distribution(cr, uid, ids, context=context)
+
+        print "END"
 
         return True
 
@@ -3111,14 +3126,20 @@ class purchase_order_line(osv.osv):
             ids = [ids]
 
         res = False
+        if self._table == "purchase_order_line":
+            print "(A)", len(ids)
 
         # [imported from the 'analytic_distribution_supply']
         # Don't save filtering data
         self._relatedFields(cr, uid, vals, context)
         # [/]
+        if self._table == "purchase_order_line":
+            print "(B)", len(ids)
 
         # Update the name attribute if a product is selected
         self._update_name_attr(cr, uid, vals, context=context)
+        if self._table == "purchase_order_line":
+            print "(C)", len(ids)
 
         if 'price_unit' in vals:
             vals.update({'old_price_unit': vals.get('price_unit')})
@@ -3131,7 +3152,11 @@ class purchase_order_line(osv.osv):
         # Remove SoQ updated flag in case of manual modification
         if not 'soq_updated' in vals:
             vals['soq_updated'] = False
+        if self._table == "purchase_order_line":
+            print "(D)", len(ids)
 
+        if self._table == "purchase_order_line":
+            print "(E)", len(ids)
         for line in self.browse(cr, uid, ids, context=context):
             new_vals = vals.copy()
             # check qty
@@ -3166,6 +3191,8 @@ class purchase_order_line(osv.osv):
                         'order_id': so_id,
                         'po_line_id': line.id,
                     }, context=context)
+        if self._table == "purchase_order_line":
+            print "(F)", len(ids)
 
         # Check the selected product UoM
         if not context.get('import_in_progress', False):
@@ -3174,6 +3201,8 @@ class purchase_order_line(osv.osv):
                     product_id = pol_read['product_id'][0]
                     uom_id = pol_read['product_uom'][0]
                     self._check_product_uom(cr, uid, product_id, uom_id, context=context)
+        if self._table == "purchase_order_line":
+            print "(G)", len(ids)
 
         return res
 
